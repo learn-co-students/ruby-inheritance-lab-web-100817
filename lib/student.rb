d667e2255b1c3e24cbe466c171a9004ed52bc2a8
github username <github email address>
2017-10-12 17:06:33 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require '/Users/Mimi/Development/code/ruby-inheritance-lab-web-100817/lib/user.rb'
+
+class Student < User
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(string)
+    @knowledge<<string
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
---
+++ lib/user.rb
@@ -0,0 +1,5 @@
+require '/Users/Mimi/Development/code/ruby-inheritance-lab-web-100817/lib/user.rb'
+class User
+  attr_accessor :first_name, :last_name
+
+end
