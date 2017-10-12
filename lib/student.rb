79a1cb19f43a1f15ad9d503f4ca5050dce705c25
Dakota Lillie <dakota.lillie@icloud.com>
2017-10-12 16:44:08 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
+
+  attr_accessor :knowledge
+
+  def initialize
+    super
+    @knowledge = Array.new
+  end
+
+  def learn(know)
+    self.knowledge << know
+  end
 
 end
