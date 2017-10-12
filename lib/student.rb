2616766970e6dfa8f7ba216e479d7394d16643fd
Fabiano Cunha <cunhasb@aol.com>
2017-10-12 17:30:54 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
-class Student 
+require './lib/user.rb'
+class Student < User
+attr_accessor :knowledge
+  def initialize
+    @knowledge=[]
+  end
 
+  def learn(brain_food)
+    self.knowledge << brain_food
+  end
 end
